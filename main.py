53f2e090e96389b81602e53a6b1106510efa6ac2
mastra90 <lukejmastrocola@gmail.com>
2022-12-05 14:41:29 +1100

    started main menu

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print ("test commit")
-print ("test third commit")
+def main_menu():
+    print ("1. View Contacts")
+    print ("2. Add Contact")
+    print ("3. Delete Contact")
+    entry = input("Please choose your option (type 1, 2 or 3)")
+
+main_menu()
