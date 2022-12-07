488cdd8e6621caeeb7df997c5c0842eb8cd0e9d1
mastra90 <lukejmastrocola@gmail.com>
2022-12-07 17:58:46 +1100

    started again. Main menu and option 1 implemented

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from view_contacts import view_contacts
-from contacts_list import all_contacts
-
 
 
 def main_menu():
@@ -8,49 +5,21 @@ def main_menu():
     print ("1. View Contacts")
     print ("2. Add Contact")
     print ("3. Delete Contact")
-    print ("4. Quit\n")
-    i = input("Please select your option (1-4): ")
-    return i
+    print ("4. Search")
+    print ("5. Quit\n")
 
+entry = {}
+user_input = 0
+main_menu()
+while user_input != "5":
 
+    user_input = (input("Type in a number (1-5): "))
 
-def add_new_contact():
-    print ("\n=========New Contact Entry=========")
-    print ("Please enter the following details:")
-    name = input("\nFull name: ")
-    phone = input("Contact number: ")
-    email = input("Email address: ")
-    
-    all_contacts ()
+    if user_input == "1":
+        print("\n======All contacts======\n")
+        for i in entry.keys():
+            print("Name: ", i, "\tNumber:", entry[i])
+        print()
 
-    print ("\nThe following entry has been added to your Contact Book:\n")
-    print(f"Name:     {name}")
-    print(f"Phone:    {phone}")
-    print(f"Email:    {email}\n")
 
     
-
-response = ""
-
-while response != "4":
-    response = main_menu()
-    if response == "1":
-        view_contacts()
-    elif response == "2":
-        add_new_contact()
-    elif response == "3":
-        print("Displays list of contacts and asks which one to delete")
-    elif response == "4":
-        continue
-    else:
-        print("Input not valid")
-    input("Please press 'Enter' to proceed")
-
-
-print("You have quit the application. Thanks!") 
-
-
-
-
-
-
--- view_contacts.py
+++
@@ -1,6 +0,0 @@
-
-def view_contacts():
-    file = open('contacts.txt', 'r')
-    read = file.read()
-    file.close
-    print(read)
