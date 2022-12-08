d52ec6fa87630dd1d6fceb5f57525bde1dd6d385
mastra90 <lukejmastrocola@gmail.com>
2022-12-08 12:22:31 +1100

    starting again. Menu implemented

--- main.py
+++ main.py
@@ -1,59 +1,13 @@
-
-
+# A function to print the main menu of the application
 def main_menu():
-    print ("\nHello! Welcome to your Contact Book.\n")
-    print ("1. View Contacts")
-    print ("2. Add Contact")
-    print ("3. Delete Contact")
-    print ("4. Search")
-    print ("5. Quit\n")
+    print("Welcome to the Address Book application!\n")
+    print("1. View all contacts")
+    print("2. Search contacts")
+    print("3. Enter new contact")
+    print("4. Edit contact")
+    print("5. Delete contact")
+    print("6. Quit application\n")
+    user_input = input("Please enter your choice (1-6): ")
+    return user_input
 
-entry = {}
-user_input = 0
 main_menu()
-while user_input != "5":
-
-    user_input = (input("Type in a number (1-5): "))
-
-    if user_input == "1":
-        print("\n======All contacts======\n")
-        for i in entry.keys():
-            print("Name: ", i, "\tNumber:", entry[i])
-        print()
-    elif user_input == "2":
-        print("\n======Add Contact======\n")
-        name = input("Full name: ")
-        phone = input("Contact number: ")
-        entry[name] = phone
-        print(f"\n{name} has been added.")
-        input("\nPress 'Enter' to continue\n")
-
-    elif user_input == "3":
-        print("\n======Lookup Number======\n")
-        name = input("Name: ")
-        if name in entry:
-            print(f"\n{name}'s number is:",entry[name])
-            print()
-        else:
-            print ()
-            print(name, "was not found")
-            print ()
-
-    elif user_input == "4":
-        print("\n======Delete Contact======\n")
-        name = input("Name of contact (case sensitive): ")
-        if name in entry:
-            del entry[name]
-            print(f"\n{name} has been removed.")
-            input("\nPress 'Enter' to continue\n") 
-        else:
-            print()
-            print(name, "was not found\n")
-            input("Press 'Enter' to continue\n")
-
-    elif user_input == "5":
-        print("\nYou have quit the application. Thanks!\n") 
-
-    else:
-        print("Invalid entry")
-        input("Press 'Enter' to continue\n")
