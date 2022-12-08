834a3ff650c3d2b2f38f44a9f01578a6174fa9d6
mastra90 <lukejmastrocola@gmail.com>
2022-12-08 13:18:33 +1100

    created formatting file and imported formatting for menu items

---
+++ formatting.py
@@ -0,0 +1,12 @@
+# Function to prompt user to press Enter to continue
+def press_enter():
+    input("Press 'Enter' to continue...")
+
+# Function to add top lines to UI
+def top_lines():
+    print("================================================================================")
+
+# Function to add bottom lines to UI
+def bot_lines():
+    print("\n================================================================================\n")
+
--- main.py
+++ main.py
@@ -1,31 +1,65 @@
+import os
+from formatting import press_enter, top_lines, bot_lines
+
 # A function to print the main menu of the application
 def main_menu():
+    os.system('cls')
     print("Welcome to the Address Book application!\n")
     print("1. View all contacts")
     print("2. Search contacts")
-    print("3. Enter new contact")
-    print("4. Edit contact")
-    print("5. Delete contact")
+    print("3. Add new contact")
+    print("4. Edit a contact")
+    print("5. Delete a contact")
     print("6. Quit application\n")
     user_input = input("Please enter your choice (1-6): ")
     return user_input
+
+# Function to clear terminal
+def clear():
+    os.system('cls')
 
 # Main program
 user_input = ""
 while user_input != "6":
     user_input = main_menu()
     if user_input == "1":
-        print("Views all contacts function")
+        clear()
+        top_lines()
+        print("---All Contacts---\n")
+        print("View all contacts - function")
+        bot_lines()
+        press_enter()
     elif user_input == "2":
-        print("Search contacts function")
+        clear()
+        top_lines()
+        print("---Search Contacts---\n")
+        print("Search contacts - function")
+        bot_lines()
+        press_enter()
     elif user_input == "3":
-        print("Enter new contacts function")
+        clear()
+        top_lines()
+        print("---Add New Contact---\n")
+        print("Add new contact - function")
+        bot_lines()
+        press_enter()
     elif user_input == "4":
-        print("Edit contacts function")
+        clear()
+        top_lines()
+        print("---Edit a Contact---\n")
+        print("Edit a contact - function")
+        bot_lines()
+        press_enter()
     elif user_input == "5":
-        print("Delete contacts function")
+        clear()
+        top_lines()
+        print("---Delete a Contact---\n")
+        print("Delete a contact - function")
+        bot_lines()
+        press_enter()
     elif user_input == "6":
-        print("Thanks for using the application!")
+        clear()
+        print("\nYou have quit the application. Goodbye!\n")
    
 
 
