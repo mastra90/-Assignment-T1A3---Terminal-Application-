a056b8931b23a8e64615ad5e847271e196faf4cd
mastra90 <lukejmastrocola@gmail.com>
2022-12-15 10:46:12 +1100

    Slight changes to run script

--- add_new_contact.py
+++ add_new_contact.py
@@ -1,16 +1,23 @@
 import os
-from formatting import top_lines, bot_lines
+from formatting import top_lines, bot_lines, press_enter
 
 # Add a new contact.
 def add_new_contact(contacts):
     print("Please enter the following details:\n")
     name = input("Full name: ")
 
-    if name == " ":
+    if name.isspace():
         os.system('clear')
         top_lines ()
-        print("\nDo not enter a space for name!\n") 
+        print("\nYou cannot enter only spaces in the name field!\n") 
         bot_lines()
+        press_enter()
+        os.system('clear')
+        top_lines()
+        text = "\nADD NEW CONTACT"
+        title = "\x1B[1;4m" + text + "\x1B[0m" + ":\n"
+        print(title)
+        add_new_contact(contacts)
     elif name in contacts:
             os.system('clear')  
             top_lines ()
--- run.sh
+++ run.sh
@@ -3,18 +3,19 @@
 # Welcome message
 echo ""
 echo "Hi! Welcome to your Contact Book!"
-read -p "Please press 'Enter' to continue..."
+read -p "Please press 'Enter' to start the application..."
 echo ""
 
 # Iterates through each Python file to give the user impression that the file is loading
 for file in *.py; do
 	echo "Running $file..."
-    sleep 0.1
+    sleep 0.05
 done
 
 # Runs the application
-sleep 1
+sleep 0.3
 echo ""
-echo "Application validtation: successful!"
-sleep 1
+echo "Application is about to start..."
+echo ""
+sleep 1.2
 python3 main_program.py
